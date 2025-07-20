2e20432ecc12eccabba7feb19af4fb9e358f5fe3
William Wall <williamjwall@gmail.com>
2025-07-19 19:10:24 -0700

    Fix deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
+  base: '/discursa_site/',
   plugins: [react()],
   server: {
     port: 3000,
@@ -23,7 +23,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': '/src'
+      '@': new URL('./src', import.meta.url).pathname
     }
   }
 }) 
